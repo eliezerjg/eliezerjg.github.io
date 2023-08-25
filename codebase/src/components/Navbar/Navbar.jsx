98ba790ad7aa9aa4efbd84aaac5edb8f04776ee0
Eliezer Garcia <71297177+eliezerjg@users.noreply.github.com>
2023-08-25 19:19:49 +0100

    correção rota

--- codebase/src/components/Navbar/Navbar.jsx
+++ codebase/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Importe o Link
 
 const menuItems = [
   { name: 'Home', href: '/' },
@@ -10,11 +11,13 @@ function Navbar() {
   return (
     <nav className="bg-gray-800 p-4 navbar">
       <div className="container mx-auto flex justify-between items-center">
-        <a href="#" className="text-white text-xl font-semibold">Eliezer Garcia</a>
+        {/* Use o Link para criar o link de navegação */}
+        <Link to="/" className="text-white text-xl font-semibold">Eliezer Garcia</Link>
         <ul className="flex space-x-4">
           {menuItems.map(item => (
             <li key={item.name}>
-              <a href={item.href} className="text-white hover:text-gray-300">{item.name}</a>
+              {/* Use o Link para criar o link de navegação */}
+              <Link to={item.href} className="text-white hover:text-gray-300">{item.name}</Link>
             </li>
           ))}
         </ul>
