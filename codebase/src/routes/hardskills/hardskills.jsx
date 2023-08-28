973d62c02c672b098cb790a5cb38485c95d9d35e
Eliezer Garcia <71297177+eliezerjg@users.noreply.github.com>
2023-08-28 12:23:22 +0100

    fix wrong words

--- codebase/src/routes/hardskills/hardskills.jsx
+++ codebase/src/routes/hardskills/hardskills.jsx
@@ -8,11 +8,11 @@ const hardskills = () => {
     <div>
       <Navbar/>
         <Content>
-        <h1 className='text-4xl font-bold mb-4'>Hard Skills</h1>
-        <p><strong>Programming Languages:</strong> Java / Javascript </p>
-        <p><strong>Frameworks / Libraries:</strong> Spring Framework, Angular / AngularJS, React + Vite</p>
-        <p><strong>Databases:</strong> PostgreSQL, MySQL e SQL Server;</p>
-        <p><strong>Tooling:</strong> Postman / Fiddler, Vscode, Intellij, Git, Android Studio and etc.</p>
+          <h1 className='text-4xl font-bold mb-4'>Hard Skills</h1>
+          <p><strong>Programming Languages:</strong> Java / Javascript </p>
+          <p><strong>Frameworks / Libraries:</strong> Spring Framework, Angular / AngularJS, React + Vite</p>
+          <p><strong>Databases:</strong> PostgreSQL, MySQL and SQL Server;</p>
+          <p><strong>Tooling:</strong> Postman / Fiddler, Vscode, Intellij, Git, Android Studio and etc.</p>
         </Content>
       <Footer/>
     </div>
